ece13eed836db6f778100f717bb59c48eb5d7a3b
Alex <alefranc@student.42.fr>
2023-01-27 19:25:14 +0100

    Proposal structure of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
+#include <iostream>
+
 #include "HTTPServer.hpp"
 
 int main(int argc, char** argv)
 {
 	if (argc != 2)
+	{
+		std::cout << "Please provide a configuration file" << std::endl;
 		return 1;
-	(void)argv;
-	HTTPServer	serv;
+	}
 
-	(void) serv;
+	try
+	{
+		HTTPServer	serv;
+		serv.parse(argv[1]);
+		serv.init(); // socket epoll etc
+		serv.run(); // infinite loop
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Server had to stop: " << e.what() << std::endl;
+		return 1;
+	}
+
 	return 0;
 }
