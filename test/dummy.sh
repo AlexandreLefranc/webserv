aa2c478927c6b020c6778c6f4f7b047e787bdb9c
Alex <alefranc@student.42.fr>
2023-03-01 16:06:33 +0100

    add example

--- test/dummy.sh
+++ test/dummy.sh
@@ -6,3 +6,5 @@ echo -n "GET / HTTP/1.1\r\nHost:example.com\r\nConnection: close\r\n\r\n"
 
 
 echo -n "GET / HTTP/1.1\r\nHost:_\r\nContent-Length:20\r\n\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+echo -n "GET /index.php?a=b&c=d HTTP/1.1\r\nhost:_\r\ncontent-length:20\r\n\r\n012345678901234567890123456789"
